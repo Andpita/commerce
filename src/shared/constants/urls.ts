9b2e13ee7f40ab7e9be54a261eb6adb444d253f2
Anderson 'Pita' Silva <dev.andpita@gmail.com>
2024-03-30 19:06:03 -0300

    change url api

--- src/shared/constants/urls.ts
+++ src/shared/constants/urls.ts
@@ -1,28 +1,28 @@
-export const URL_USER = 'https://api-ec.andpita.online/user';
-export const URL_USER_ALL = 'https://api-ec.andpita.online/user/all';
-export const URL_USER_INSERT_ADM = 'https://api-ec.andpita.online/user/admin';
+export const URL_USER = 'https://ec-api.andpita.shop/user';
+export const URL_USER_ALL = 'https://ec-api.andpita.shop/user/all';
+export const URL_USER_INSERT_ADM = 'https://ec-api.andpita.shop/user/admin';
 
-export const URL_AUTH = 'https://api-ec.andpita.online/auth';
+export const URL_AUTH = 'https://ec-api.andpita.shop/auth';
 
-export const URL_PRODUCTS = 'https://api-ec.andpita.online/product';
-export const URL_PRODUCTS_ID = 'https://api-ec.andpita.online/product/{id}';
+export const URL_PRODUCTS = 'https://ec-api.andpita.shop/product';
+export const URL_PRODUCTS_ID = 'https://ec-api.andpita.shop/product/{id}';
 
-export const URL_CATEGORY = 'https://api-ec.andpita.online/category';
-export const URL_CATEGORY_ID = 'https://api-ec.andpita.online/category/{id}';
+export const URL_CATEGORY = 'https://ec-api.andpita.shop/category';
+export const URL_CATEGORY_ID = 'https://ec-api.andpita.shop/category/{id}';
 
-export const URL_ORDER = 'https://api-ec.andpita.online/order';
-export const URL_ORDER_ALL = 'https://api-ec.andpita.online/order/all';
-export const URL_ORDER_ID = 'https://api-ec.andpita.online/order/{orderId}';
+export const URL_ORDER = 'https://ec-api.andpita.shop/order';
+export const URL_ORDER_ALL = 'https://ec-api.andpita.shop/order/all';
+export const URL_ORDER_ID = 'https://ec-api.andpita.shop/order/{orderId}';
 
-export const URL_CART = 'https://api-ec.andpita.online/cart';
-export const URL_PAYMENTS = 'https://api-ec.andpita.online/order';
+export const URL_CART = 'https://ec-api.andpita.shop/cart';
+export const URL_PAYMENTS = 'https://ec-api.andpita.shop/order';
 
-export const URL_ADDRESS = 'https://api-ec.andpita.online/address';
+export const URL_ADDRESS = 'https://ec-api.andpita.shop/address';
 
-export const URL_STATE = 'https://api-ec.andpita.online/state';
-export const URL_CITY = 'https://api-ec.andpita.online/city/{id}';
-export const URL_CEP = 'https://api-ec.andpita.online/correios/{id}';
+export const URL_STATE = 'https://ec-api.andpita.shop/state';
+export const URL_CITY = 'https://ec-api.andpita.shop/city/{id}';
+export const URL_CEP = 'https://ec-api.andpita.shop/correios/{id}';
 
-export const URL_TICKET = 'https://api-ec.andpita.online/ticket';
+export const URL_TICKET = 'https://ec-api.andpita.shop/ticket';
 
 export const FRETE_CALC = 'httpss://portal.kangu.com.br/tms/transporte/simular';
