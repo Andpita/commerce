97d607e980b7c65f10ec4ab68b1288f074a993db
Anderson 'Pita' Silva <dev.andpita@gmail.com>
2024-03-30 15:38:05 -0300

    fix url call

--- src/shared/constants/urls.ts
+++ src/shared/constants/urls.ts
@@ -1,28 +1,28 @@
-export const URL_USER = 'http://localhost:4003/user';
-export const URL_USER_ALL = 'http://localhost:4003/user/all';
-export const URL_USER_INSERT_ADM = 'http://localhost:4003/user/admin';
+export const URL_USER = 'https://api-ec.andpita.online/user';
+export const URL_USER_ALL = 'https://api-ec.andpita.online/user/all';
+export const URL_USER_INSERT_ADM = 'https://api-ec.andpita.online/user/admin';
 
-export const URL_AUTH = 'http://localhost:4003/auth';
+export const URL_AUTH = 'https://api-ec.andpita.online/auth';
 
-export const URL_PRODUCTS = 'http://localhost:4003/product';
-export const URL_PRODUCTS_ID = 'http://localhost:4003/product/{id}';
+export const URL_PRODUCTS = 'https://api-ec.andpita.online/product';
+export const URL_PRODUCTS_ID = 'https://api-ec.andpita.online/product/{id}';
 
-export const URL_CATEGORY = 'http://localhost:4003/category';
-export const URL_CATEGORY_ID = 'http://localhost:4003/category/{id}';
+export const URL_CATEGORY = 'https://api-ec.andpita.online/category';
+export const URL_CATEGORY_ID = 'https://api-ec.andpita.online/category/{id}';
 
-export const URL_ORDER = 'http://localhost:4003/order';
-export const URL_ORDER_ALL = 'http://localhost:4003/order/all';
-export const URL_ORDER_ID = 'http://localhost:4003/order/{orderId}';
+export const URL_ORDER = 'https://api-ec.andpita.online/order';
+export const URL_ORDER_ALL = 'https://api-ec.andpita.online/order/all';
+export const URL_ORDER_ID = 'https://api-ec.andpita.online/order/{orderId}';
 
-export const URL_CART = 'http://localhost:4003/cart';
-export const URL_PAYMENTS = 'http://localhost:4003/order';
+export const URL_CART = 'https://api-ec.andpita.online/cart';
+export const URL_PAYMENTS = 'https://api-ec.andpita.online/order';
 
-export const URL_ADDRESS = 'http://localhost:4003/address';
+export const URL_ADDRESS = 'https://api-ec.andpita.online/address';
 
-export const URL_STATE = 'http://localhost:4003/state';
-export const URL_CITY = 'http://localhost:4003/city/{id}';
-export const URL_CEP = 'http://localhost:4003/correios/{id}';
+export const URL_STATE = 'https://api-ec.andpita.online/state';
+export const URL_CITY = 'https://api-ec.andpita.online/city/{id}';
+export const URL_CEP = 'https://api-ec.andpita.online/correios/{id}';
 
-export const URL_TICKET = 'http://localhost:4003/ticket';
+export const URL_TICKET = 'https://api-ec.andpita.online/ticket';
 
-export const FRETE_CALC = 'https://portal.kangu.com.br/tms/transporte/simular';
+export const FRETE_CALC = 'httpss://portal.kangu.com.br/tms/transporte/simular';
